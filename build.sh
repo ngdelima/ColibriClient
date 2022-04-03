4bce950056ca07d835c21ae90e454a59c9588930
ngdelima <delimanicolasgustavo@gmail.com>
2022-04-02 23:54:08 -0300

    Adapted project to Linux enviroment

---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+msgToPrint=""
+
+printMsg()
+{
+	echo "**----------------------**"
+	echo ""
+	echo $msgToPrint
+	echo ""
+	echo "**----------------------**"
+}
+
+build()
+{
+	#Go to build directory
+	mkdir -p build
+	cd build
+
+	msgToPrint="Generating make files..."
+	printMsg
+
+	cmake -DCMAKE_BUILD_TYPE=Debug ..
+
+	msgToPrint="Building..."
+	printMsg
+
+	cmake --build . && cp -r ../resources/ ../bin/
+
+	#Go back to previous directory once we finish
+	cd ..
+}
+
+clean()
+{
+	if [ -d "./bin/" ]
+	then
+		rm -rf ./bin
+	fi
+
+	if [ -d "./build/" ]
+	then
+		rm -rf ./build
+	fi
+}
+
+run()
+{
+	if [ -d "./bin/" ] && [ -f "./bin/colibriClient" ]
+	then
+		./bin/colibriClient
+	fi
+}
+
+
+case $1 in
+	"")
+		build
+		;;
+	build)
+		build
+		;;
+	clean)
+		clean
+		;;
+	run)
+		run
+		;;
+esac
+	
---
+++ include/Listener.h
@@ -0,0 +1,20 @@
+#ifndef LISTENER_H
+#define LISTENER_H
+
+#include"InputEvent.h"
+
+class Listener
+{
+public:
+	virtual ~Listener() {}
+	virtual void onNotify(INPUT_EVENT _event) = 0;
+};
+
+class Publisher
+{
+public:
+	virtual void addListener(Listener* _listener) = 0;
+	virtual void notify(INPUT_EVENT _event) = 0;
+};
+
+#endif
